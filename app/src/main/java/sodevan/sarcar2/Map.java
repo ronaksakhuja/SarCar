5316ae08550bd9df2a58cfc5c44033bb025a6732
ronaksakhuja@gmail.com <ronaksakhuja@gmail.com>
2017-04-12 23:20:32 +0530

    Added Fused Location

--- app/src/main/java/sodevan/sarcar2/Map.java
+++ app/src/main/java/sodevan/sarcar2/Map.java
@@ -1,13 +1,39 @@
 package sodevan.sarcar2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Map extends AppCompatActivity {
-
+    final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+
+        final FusedLocation fusedLocation = new FusedLocation(context, new FusedLocation.Callback(){
+            @Override
+            public void onLocationResult(Location location){
+                //Do as you wish with location here
+                Log.d("TAG","loc:"+location.getLatitude()+" long : "+location.getLongitude());
+            }
+        });
+        TimerTask t=new TimerTask() {
+            @Override
+            public void run() {
+                fusedLocation.getCurrentLocation(3);
+
+            }
+        };
+
+        Timer time=new Timer();
+        time.schedule(t,0,5000);
+
     }
 }
