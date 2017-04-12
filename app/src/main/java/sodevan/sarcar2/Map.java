640d87d5262d843ea9c44890c07b66bb4728f760
GAGAN <gagankishor98@gmail.com>
2017-04-13 01:09:35 +0530

    algo

--- app/src/main/java/sodevan/sarcar2/Map.java
+++ app/src/main/java/sodevan/sarcar2/Map.java
@@ -2,6 +2,7 @@ package sodevan.sarcar2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Map extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Map extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        Algo a=null;
+        boolean b=a.CollisionChecker(51.5031730,-0.1265895,51.5032131,-0.1256239,51.5032264,-0.1274478,51.5031329,-0.1241648);
+        Log.i("Value: ",""+b);
     }
 }
