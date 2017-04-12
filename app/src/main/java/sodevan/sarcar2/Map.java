332bca12de381b0f195334e89529501c66a46c16
ravipiyush <piyush.gupta111019@gmail.com>
2017-04-12 23:22:22 +0530

    Fixed Map Fragment

--- app/src/main/java/sodevan/sarcar2/Map.java
+++ app/src/main/java/sodevan/sarcar2/Map.java
@@ -1,13 +1,41 @@
 package sodevan.sarcar2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 
-public class Map extends AppCompatActivity {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap gmap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 17);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map2);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap ;
+
+
     }
 }
